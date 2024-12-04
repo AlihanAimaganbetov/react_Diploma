571e9443fec01fc0c3d537f2050c97d251d4369a
aliha <kevingjames.11@gmail.com>
2024-12-04 17:00:30 +0500

    123

--- src/components/Form.js
+++ src/components/Form.js
@@ -73,42 +73,49 @@ function Form() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h3>Введите адреса начальной и конечной точки:</h3>
-        <label>
-          Начальный адрес:
-          <input
-            type="text"
-            value={startAddress}
-            onChange={(e) => setStartAddress(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Конечный адрес:
-          <input
-            type="text"
-            value={endAddress}
-            onChange={(e) => setEndAddress(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <button type="submit" disabled={loading}>Отправить</button>
-      </form>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h3>Введите адреса начальной и конечной точки:</h3>
+          <label>
+            Начальный адрес:
+            <input
+                type="text"
+                value={startAddress}
+                onChange={(e) => setStartAddress(e.target.value)}
+                required
+            />
+          </label>
+          <br />
+          <label>
+            Конечный адрес:
+            <input
+                type="text"
+                value={endAddress}
+                onChange={(e) => setEndAddress(e.target.value)}
+                required
+            />
+          </label>
+          <br />
+          <button type="submit" disabled={loading}>Отправить</button>
+        </form>
 
-      {loading && <p>Пожалуйста, подождите... Идет обработка данных.</p>}
-      {error && <p style={{ color: 'red' }}>Ошибка: {error}</p>}
-      {result && (
-        <div>
-          <h3>Результаты:</h3>
-          <p>Путь к карте: <a href={`http://127.0.0.1:5000//${result.map_path}`} target="_blank" rel="noopener noreferrer">Посмотреть карту</a></p>
-        </div>
-      )}
-    </div>
+        {loading && <p>Пожалуйста, подождите... Идет обработка данных.</p>}
+        {error && <p style={{ color: 'red' }}>Ошибка: {error}</p>}
+        {result && (
+            <div>
+              <h3>Результаты:</h3>
+              <p>Карта маршрута:</p>
+              <iframe
+                  src={`http://127.0.0.1:5000/${result.map_path}`}
+                  title="Карта маршрута"
+                  style={{ width: '800px', height:   '800px', border: '1px solid black' }}
+                  allowFullScreen
+              />
+            </div>
+        )}
+      </div>
   );
+
 }
 
 export default Form;
